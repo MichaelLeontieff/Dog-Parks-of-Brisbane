64230e4cecd296b6297a59b90cc7bc798426e279
Michael Leontieff-Smith <michael@leontieff.net>
2016-04-29 23:57:30 +1000

    Added commenting to function headers

--- myscripts.js
+++ myscripts.js
@@ -1,23 +1,30 @@
-
+/*
+Initialise map object and marker corresponding to lat lon of park
+ */
 function initialize() {
-  var mapOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(-27.38123113, 153.0436597)
-  }
-  var map = new google.maps.Map(document.getElementById("locationmap"), mapOptions);
 
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(-27.38123113, 153.0436597),
-    map: map,
-    title: 'Dog Park'
-  });
+    var mapOptions = {zoom: 15, center: new google.maps.LatLng(-27.38123113, 153.0436597)};
+
+    var map = new google.maps.Map(document.getElementById("locationmap"), mapOptions);
+
+    var marker;
+
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(-27.38123113, 153.0436597), map: map, title: 'Dog Park'
+    });
 }
 
+/*
+Load script
+ */
 function loadScript() {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDYduLoGeuBaC8Cc0fOPMXOBBljvQkxHFk&callback=initialize";
-  document.body.appendChild(script);
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDYduLoGeuBaC8Cc0fOPMXOBBljvQkxHFk&callback=initialize";
+    document.body.appendChild(script);
 }
 
+/*
+Run when page loads
+ */
 window.onload = loadScript;
